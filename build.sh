016088cd25e6890478f21722e35d953bb5438884
Replit user <replituser@example.com>
2021-03-31 11:21:15 +0000

    small fix for PATH setup in build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-[[ "$OSTYPE" == "msys" ]] && PATH="/c/Program Files/Mono/bin/mono:$PATH"
+[[ "$OSTYPE" == "msys" ]] && PATH="/c/Program Files/Mono/bin/:$PATH"
 
 shopt -s extglob
 mcs -out:nettype.exe !(*sample).cs
